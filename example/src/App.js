c19ba62d0d58b6c98b9a17c8265e9c8bf75f9416
mitnik <mitnik@mitnik.mitnik.com>
2022-05-23 17:50:49 -0500

    stable version ready for testing on third party projects

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { ExampleComponent } from 'react-generic-pagination'
 import 'react-generic-pagination/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [page , setPage] = useState(1)
+  const helloWorld = (page) => {
+    setPage(page)
+    console.log('pagina', page)
+  }
+  
+  return <ExampleComponent pagina={page} total={10} hello={helloWorld}/>
 }
 
 export default App
