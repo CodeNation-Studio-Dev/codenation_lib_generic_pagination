01e38a678b5d777052e11fb13a0a9d6f0fd6d203
mitnik <mitnik@mitnik.mitnik.com>
2022-05-23 17:54:32 -0500

    stable version ready for testing on third party projects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './styles.module.css'
+import Pagination, { paginationClasses } from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+
+export const ExampleComponent = (props) => {
+
+  const [page, setPage] = useState('');
+
+  const handleChange = (event, value) => {
+    console.log(value)
+    props.hello(value)
+    setPage(value);
+  };
+
+  return (
+    <Stack spacing={2}>
+      <Pagination count={props.total} onChange={handleChange} showFirstButton showLastButton/>
+    </Stack>
+  );
 }
